dfab7c702f8a91f516210fde208ac45146ad10cb
melchr <melchrstmn@gmail.com>
2020-07-15 13:38:10 -0400

    added serializer to index method

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MaterialsController < ApplicationController
     def index
         materials = Material.all
-        render json: materials
+        render json: MaterialSerializer.new(materials)
     end
 
     def create
