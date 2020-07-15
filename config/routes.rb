3b026326778e2edef479a59b76c3364e983b2a94
melchr <melchrstmn@gmail.com>
2020-07-15 13:12:29 -0400

    added create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :materials, only: [:index]
+      resources :materials, only: [:index, :create]
     end
   end
 end
