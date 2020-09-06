3946f7f7f81bae1f33b2c798de431bd5c08fa639
melchr <melchrstmn@gmail.com>
2020-09-06 15:00:48 -0400

    commented out unnecessary code

--- app/serializers/material_serializer.rb
+++ app/serializers/material_serializer.rb
@@ -1,5 +1,5 @@
 class MaterialSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :description, :url, :category_id, :category
-  belongs_to :category
+  #belongs_to :category
 end
